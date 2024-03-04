2de170e81774533319a8545c9dc50cf1a5f1dee3
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-03-04 11:32:54 +0800

    added

--- mvc/app/core/Database.php
+++ mvc/app/core/Database.php
@@ -4,20 +4,21 @@ class Database
 {
     public function connect()
     {
-        $string = "mysql:host=localhost;dbname=mvc_bsis3c";
+        //user - id, firstname, lastname, email, password
+        $string = "mysql:host=localhost;dbname=jjic";
         $con = new PDO($string, 'root', '');
         return $con;
     }
 
     public function query ($query, $data =[])
     {
-        $con = this->connect();
+        $con = $this->connect();
         $stm = $con->prepare($query);
 
         $check = $stm->execute($data);
 
         if ($check) {
-            $result = $stm->fetchAll(PDO: :FETCH_OBJ);
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
 
             if (is_array($result) && count($result) > 0) {
                 return $result;
--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -3,4 +3,5 @@
 require 'functions.php';
 require 'Database.php';
 require 'Controller.php';
+require 'Model.php';
 require 'App.php';
