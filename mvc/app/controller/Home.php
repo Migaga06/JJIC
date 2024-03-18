ce21aa04aea099fa4ccaa7b624c1a21f5ecffc7b
BARTSSSsss <justiinebartolome.jbe@gmail.com>
2024-03-18 12:07:35 +0800

    aaa

--- mvc/app/controller/Home.php
+++ mvc/app/controller/Home.php
@@ -4,22 +4,12 @@ class Home extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
      $model = new Model();
      $arr['firstname'] = 'John Bren';
      $arr2['lastname'] = 'Urbano';
      $data = $model->where($arr, $arr2);
-     show($data);
+     show($data); git
 
      $this->view('home');
-=======
-        $model = new Model();
-        $arr['firstname'] = 'Bren';
-        $arr2['lastname'] = 'Bding';
-        $data = $model->where($arr, $arr2);
-        show($data);
-
-        $this->view('home');
->>>>>>> 1b1d742ef3d1b7e7b6bd49a236a5d6925aff0dd7
     }
 }
