2091925108eebe45db782e31944061b3c98aca01
Joseph Urbano <josephurbano0904@gmail.com>
2024-04-21 23:27:02 +0800

    add mvc-test

---
+++ mvc-test/app/core/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT', 'http://localhost/JJIC/mvc-test/public');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc_bsis3c');
+define('DB_USER', 'root');
+define('DB_PASS', '');
