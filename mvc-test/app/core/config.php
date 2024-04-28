12f3e101bbcda20ba2ec2328ce5cdaf511d0b059
ivanreb017 <158011956+ivanreb017@users.noreply.github.com>
2024-04-28 21:40:32 +0800

    mvc-test
    assignment

---
+++ mvc-test/app/core/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT', 'http://localhost/JJIC/mvc-test/public');
+define('PATH','E:/xampp/htdocs/JJIC/mvc-test/app/views/');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tasks_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
